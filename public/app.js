004e3dd756a6c6ccd9bcddb04661aaddd2a58bd2
douglasNordenstedt <douglas.nordenstedt@hotmail.com>
2023-10-25 12:30:33 +0200

    first commit, simple airtable base and favicon

---
+++ public/app.js
@@ -0,0 +1,33 @@
+// Definiera URL:en till din server
+const url = 'http://localhost:4242/contacts';
+
+// Använd fetch för att skicka en GET-förfrågan till servern
+fetch(url)
+    .then(response => {
+        // Kontrollera om förfrågan var framgångsrik
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();  // Konvertera svaret till JSON
+    })
+    .then(data => {
+        // Hämta div-elementet där kontaktpersonerna ska visas
+        const contactsDiv = document.getElementById('contacts');
+
+        // Skapa en ul-element för att lista kontaktpersonerna
+        const ul = document.createElement('ul');
+
+        // Loopa igenom datan och skapa ett li-element för varje kontaktperson
+        data.forEach(contact => {
+            const li = document.createElement('li');
+            li.textContent = `${contact.name} - ${contact.email}`;
+            ul.appendChild(li);
+        });
+
+        // Lägg till ul-elementet i div-elementet
+        contactsDiv.appendChild(ul);
+    })
+    .catch(error => {
+        // Logga eventuella fel till konsolen
+        console.error('There has been a problem with your fetch operation:', error);
+    });
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const Airtable = require('airtable');
+
+const app = express();
+const base = new Airtable({ apiKey: 'patbJ1CKcZTZ8Vb1y.f4bf3258c78e56c125da719e713b3b18b6c9cbf957e3909d21d300fca6228b47' }).base('appGRW6nt8t4UQu8h');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile('./public/index')
+})
+
+
+// Definiera en route för att hämta kontaktpersoner
+app.get('/contacts', (req, res) => {
+    // Skapa en tom array för att lagra kontaktpersoner
+    let contacts = [];
+    
+    // Hämta data från Airtable
+    base('contacts').select({
+        view: 'Grid view'
+    }).eachPage((records, fetchNextPage) => {
+        // Lägg till varje post i contacts-arrayen
+        records.forEach(record => {
+            contacts.push({
+                id: record.id,
+                name: record.get('Name'),
+                email: record.get('Email')
+            });
+        });
+        // Hämta nästa sida med poster
+        fetchNextPage();
+    }, (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Server Error');
+            return;
+        }
+        // Skicka kontaktpersonerna som ett JSON-svar
+        res.json(contacts);
+    });
+});
+
+
+// Starta servern
+app.listen(4242, () => {
+    console.log('Server is running on http://localhost:4242');
+});
